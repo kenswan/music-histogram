d7d75ee920e295049c6447b7a851969600639934
Ken Swan <49496839+kenswan@users.noreply.github.com>
2022-06-15 13:35:15 -0500

    Swagger, Build Props & Centralized Package Versions (#11)

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,21 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+﻿var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
