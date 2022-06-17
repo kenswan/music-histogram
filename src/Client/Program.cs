a5ada499d3312d5a1656cf8698ceb7f07b37af74
Kenneth Swan <49496839+kenswan@users.noreply.github.com>
2022-06-17 17:40:20 -0500

    add rest client to access api

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,3 +1,4 @@
+﻿using BlazorFocused;
 using BlazorMusic.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var hostEnvironmentBaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = hostEnvironmentBaseAddress });
+
+builder.Services.AddRestClient(client =>
+{
+    client.BaseAddress = hostEnvironmentBaseAddress;
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 await builder.Build().RunAsync();
