34b7c118935df3d54d0119f7a8cec6547bc6ef0e
Kenneth Swan <49496839+kenswan@users.noreply.github.com>
2022-06-19 02:05:21 -0500

    histogram updates - WIP

---
+++ src/Client/Reducers/HistogramDataReducer.cs
@@ -0,0 +1,36 @@
+﻿using BlazorFocused;
+using BlazorMusic.Client.Models;
+
+namespace BlazorMusic.Client.Reducers;
+
+public class HistogramDataReducer : IReducer<ArtistStore, HistorgramData>
+{
+    public HistorgramData Execute(ArtistStore input)
+    {
+        Dictionary<int, int> counts = new();
+        HistorgramData data = new();
+
+        if (input.Releases is not null)
+        {
+            foreach (var release in input.Releases)
+            {
+                foreach (var track in release.Tracks)
+                {
+                    var hasKey = int.TryParse(track.ReleaseDate, out int key);
+                    if (hasKey)
+                    {
+                        if (counts.ContainsKey(int.Parse(track.ReleaseDate)))
+                            counts[key]++;
+                        else
+                            counts.Add(key, 1);
+                    }
+                }
+            }
+
+            data.Years = counts.Keys.ToArray();
+            data.Releases = counts.Values.ToArray();
+        }
+
+        return data;
+    }
+}
